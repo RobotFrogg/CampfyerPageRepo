33b628aa16c500b456308155652842d30816c97f
Ian Herzel <44041122+TheEnsphere@users.noreply.github.com>
2021-09-19 13:14:55 +0000

    actually final push.

--- service-worker.js
+++ service-worker.js
@@ -2,6 +2,34 @@ const cacheName = 'Offline Cache';
 
 const cacheAssets = [
     'index.html',
+    'service-worker.js',
+    '.assets/bootstrap/css/bootstrap.min.css',
+    '.assets/bootstrap/js/bootstrap.min.js',
+    '.assets/css/dat-gui-light-theme.css',
+    '.assets/css/Lightbox-Gallery.css',
+    '.assets/css/style.css',
+    '.assets/css/Team-Boxed.css',
+
+    '.assets/fonts/font-awesome.min.css',
+    '.assets/fonts/fontawesome-webfont.eot',
+    '.assets/fonts/fontawesome-webfont.svg',
+    '.assets/fonts/fontawesome-webfont.ttf',
+    '.assets/fonts/fontawesome-webfont.woff',
+    '.assets/fonts/fontawesome-webfont.woff2',
+    '.assets/fonts/FontAwesome.otf',
+
+    '.assets/img/3.jpg',
+    '.assets/img/Animation.png',
+    '.assets/img/appealing-shape (5).png',
+    '.assets/img/duckjames.png',
+    '.assets/img/Illustraion2.png',
+    '.assets/img/Illustration3.png',
+    '.assets/img/turtjessie.png',
+    '.assets/img/unknown.png',
+
+    '.assets/js/Lightbox-Gallery.js',
+    '.assets/img/script.js',
+    '.assets/img/stylish-portfolio.js'
 
 ]
 

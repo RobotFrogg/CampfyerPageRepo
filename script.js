96695c138f786b6b038f677d84ce383a87b686e0
Ian Herzel <44041122+TheEnsphere@users.noreply.github.com>
2021-09-19 07:30:01 +0000

    Working Code Before Merge

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const s = function( canvas ) {
     let y
     //setup canvas
     canvas.setup = function() {
-        canvas.createCanvas(canvas.windowWidth, canvas.windowHeight)
+        canvas.createCanvas(canvas.windowWidth, canvas.windowHeight).parent("canvasDiv")
+
+
         canvas.rectMode(canvas.CENTER)
         canvas.colorMode(canvas.HSB)
         canvas.background('#f7f7f7')
@@ -24,13 +26,13 @@ const s = function( canvas ) {
 
     //draws canvas
     canvas.draw = function() {
-        let { min_size, max_size, radius, styles, draw_range, c_range, c_shift, saturation, brightness, light_dither, opacity, opacity_dither, bg_colour} = controls
+        let { min_size, max_size, radius, styles, draw_range, colour_dither, colour, saturation, brightness, light_dither, opacity, opacity_dither, bg_colour} = controls
 
         //brush size
         size = canvas.random(min_size, max_size)
 
-        let hue = canvas.floor(canvas.random(c_range)+c_shift)
-        //^this isn't working as intended: c_range + c_shift equals number higher than intended
+        let hue = canvas.floor(canvas.random(colour_dither)+colour)
+        //^this isn't working as intended: colour_dither + colour equals number higher than intended
 
         //determines style
         if (styles == 'fill') {
@@ -48,8 +50,8 @@ const s = function( canvas ) {
             canvas.rect(x, y, canvas.random(size-5, size), canvas.random(size-3, size), 0)
         }else if(controls.pause && mouseDown){  
           //enables user to draw with mouse when paused
-            x = canvas.mouseX
-            y = canvas.mouseY
+             x = canvas.mouseX
+             y = canvas.mouseY
             canvas.rect(x, y, canvas.random(size-5, size), canvas.random(size-3, size), 0)
         }
           
@@ -57,7 +59,7 @@ const s = function( canvas ) {
           
         //randomizes auto drawing
         let r = canvas.floor(canvas.random(5))
-        if(!mouseDown){
+
           switch(r) {
             case 0: 
               x = (x + draw_range) % canvas.windowWidth 
@@ -72,7 +74,7 @@ const s = function( canvas ) {
               y = Math.abs( (y - draw_range) % canvas.windowHeight )
               break
           }
-        }
+        
     }
 }
 
@@ -85,8 +87,8 @@ const Controls = function() {
 
     this.draw_range = 30
 
-    this.c_range = 30
-    this.c_shift = 0
+    this.colour_dither = 30
+    this.colour = 0
     this.saturation = 0
 
     this.brightness = 80
@@ -112,20 +114,18 @@ window.onload = function() {
     folderBrush.add(controls, 'styles', [ 'fill', 'nocolour'] );
 
     let folderColour = gui.addFolder('Colour Settings');
-    folderColour.add(controls, 'c_range', 0, 360)
-    folderColour.add(controls, 'c_shift', 0, 360)
+    folderColour.add(controls, 'colour', 0, 360)
+    folderColour.add(controls, 'colour_dither', 0, 360)
     folderColour.add(controls, 'saturation', 0, 100)
     folderColour.add(controls, 'brightness', 0, 100)
     folderColour.add(controls, 'light_dither', 0, 10)
-    folderColour.add(controls, 'light_dither', 0, 10)
-    folderColour.addColor(controls, 'bg_colour')
+    folderColour.add(controls, 'opacity', 0, 1)
+    folderColour.add(controls, 'opacity_dither', 0, 10)
+
 
     let folderAutoDraw = gui.addFolder('Auto Draw Settings')
 
     folderAutoDraw.add(controls, 'draw_range', 1, 100)
-    folderAutoDraw.add(controls, 'light_dither', 0, 10)
-    folderAutoDraw.add(controls, 'opacity', 0, 1)
-    folderAutoDraw.add(controls, 'opacity_dither', 0, 10)
     pause = folderAutoDraw.add(controls, 'pause')
     folderAutoDraw.add(controls, 'save')
 }
